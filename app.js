4f3129f03cbecc670d988c9bbebf579c8da97406
IamGur <36259923+IamGur@users.noreply.github.com>
2018-08-28 18:27:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -502,6 +502,42 @@ if (command === 'serverinfo' || command === 'si') {
     message.channel.send(`*${warnUser.user.tag} has been warned*`)
 
   }
+  if (command === 'mute') {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You can't warn someone.**");
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!tomute) return message.reply("**Mention a user to mute**");
+    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("oof");
+    let muterole = message.guild.roles.find(`name`, "muted");
+
+    if (!muterole) {
+        try {
+            muterole = await message.guild.createRole({
+                name: "Muted",
+                color: "#000000",
+                permissions: []
+            })
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+
+    let mutetime = args[1];
+    if (!mutetime) return message.reply("You didn't specify a time!");
+
+    await (tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+    setTimeout(function() {
+        tomute.removeRole(muterole.id);
+        message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(mutetime));
+}
   if (command === 'help') {
 
     let embed = new Discord.RichEmbed()
