7e32e1aef7c14b4da5aafb3a8dad99712f861022
IamGur <36259923+IamGur@users.noreply.github.com>
2019-03-20 19:41:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -921,7 +921,6 @@ client.on('guildMemberRemove', member => {
    guild.channels.get('557926166082617345').setName(`Total Members: ${guild1.members.size}`)
    guild.channels.get('557926250543579136').setName(`Bots: ${guild1.members.filter((members) => members.user.bot).size}`)
    guild.channels.get('557926364255354900').setName(`Humans: ${guild1.members.filter(member => !member.user.bot).size}`);
-	  });
 	  /*const guild = client.guilds.get('506005517425180672');
    guild.channels.get('529207972732207104').setName(`Total Members: ${guild.members.size}`);
    guild.channels.get('529208080571826188').setName(`Total Bots: ${guild.members.filter((members) => members.user.bot).size} `);
